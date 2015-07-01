9945916efd22b02b12c9e0ca5cf5bfbd3f77833b
Kitalken Hell <kitalkenhell@gmail.com>
2015-07-01 15:24:56 +0200

    first commit

---
+++ Assets/FlameController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlameController : MonoBehaviour
+{
+    void Start()
+    {
+        //GetComponent<Animator>().speed = Random.Range(1, 10);
+        GetComponent<Animator>().SetFloat(Animator.StringToHash("offset"), Random.Range(0, 1.0f));
+    }
+
+    void Update()
+    {
+
+    }
+}
