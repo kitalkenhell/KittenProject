28687c4b8bcc28fa2439095dc678b8fd762fbc51
gotard <mlaszcz@wi.zut.edu.pl>
2015-07-26 00:07:20 +0200

    -Wstępna wersja poruszających się platform -Alfabetyczne sortowanie hierarchii

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour 
+{
+    public float speed = 10.0f;
+    public float acceleration = 2.0f;
+    public float stoppingDistance = 2.0f;
+    public float waitTime = 0.0f;
+
+    void OnDrawGizmos()
+    {
+        const float size = 1.5f;
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(transform.position, new Vector3(size, size, size));
+    }
+}
