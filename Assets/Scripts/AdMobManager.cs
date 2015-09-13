8e01fba65015b4f6c99ea6d052f0ab7c55269cf9
gotard <mlaszcz@wi.zut.edu.pl>
2015-09-14 00:43:59 +0200

    reklamy

---
+++ Assets/Scripts/AdMobManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using GoogleMobileAds.Api;
+
+public class AdMobManager : MonoBehaviour
+{
+    InterstitialAd interstitial;
+    bool visible = false;
+
+    void Start()
+    {
+        //BannerView bannerView = new BannerView("ca-app-pub-2026328596529981/9822243153", AdSize.Banner, AdPosition.Top);
+        //AdRequest request = new AdRequest.Builder().Build();
+        //bannerView.LoadAd(request);
+
+        interstitial = new InterstitialAd("ca-app-pub-2026328596529981/1101444759");
+        AdRequest iRequest = new AdRequest.Builder().Build();
+        interstitial.LoadAd(iRequest);
+
+        visible = false;
+    }
+
+    void Update()
+    {
+        if (interstitial.IsLoaded() && !visible)
+        {
+            interstitial.Show();
+            visible = true;
+        }
+    }
+}
