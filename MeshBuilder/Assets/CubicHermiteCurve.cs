71af91898a910442033cdfa114b34d6c84c6c0b5
Marcin Łaszcz <ml@byss.mobi>
2015-09-28 17:16:51 +0200

    krzywe

---
+++ MeshBuilder/Assets/CubicHermiteCurve.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CubicHermiteCurve : MonoBehaviour
+{
+    public Vector2 begin = Vector2.zero;
+    public Vector2 end = Vector2.one;
+    public Vector2 tangentBegin = Vector2.left;
+    public Vector2 tangentEnd = Vector2.right;
+
+    public List<Vector2> path;
+
+    public void Refresh()
+    {
+        path.Clear();
+        path.Add(begin);
+        int steps = 20;
+
+        for (int t = 0; t < steps; t++)
+        {
+            float s = (float)t / (float)steps;   
+            float h1 = 2 * s * s * s - 3 * s * s + 1;          
+            float h2 = -2 * s * s * s + 3 * s * s;             
+            float h3 = s * s * s - 2 * s * s * s + s;         
+            float h4 = s * s * s - s * s;
+            Vector2 p = h1 * begin +  h2 * end + h3 * tangentBegin + h4 * tangentEnd;
+
+            path.Add(p);
+        }
+    }
+
+    public void Reset()
+    {
+        begin = Vector2.zero;
+        end = Vector2.one;
+        tangentBegin = Vector2.left;
+        tangentEnd = Vector2.right;
+    }
+}
---
+++ MeshBuilder/Assets/Editor/CubicHermiteCurveEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(CubicHermiteCurve))]
+public class CubicHermiteCurveEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        CubicHermiteCurve curve = (CubicHermiteCurve)target;
+
+        if (GUILayout.Button("Reset"))
+        {
+            curve.Reset();
+        }
+    }
+
+    public void OnSceneGUI()
+    {
+        CubicHermiteCurve curve = (CubicHermiteCurve)target;
+
+        Handles.color = Color.white;
+        curve.begin = Handles.FreeMoveHandle(curve.begin, Quaternion.identity, 0.25f, Vector3.zero, Handles.DotCap);
+        curve.end = Handles.FreeMoveHandle(curve.end, Quaternion.identity, 0.25f, Vector3.zero, Handles.DotCap);
+
+        Handles.color = Color.red;
+        curve.tangentBegin = Handles.FreeMoveHandle(curve.begin + curve.tangentBegin, Quaternion.identity, 0.25f, Vector3.zero, Handles.DotCap) - 
+            new Vector3(curve.begin.x, curve.begin.y, 0);
+        curve.tangentEnd = Handles.FreeMoveHandle(curve.end + curve.tangentEnd, Quaternion.identity, 0.25f, Vector3.zero, Handles.DotCap) - 
+            new Vector3(curve.end.x, curve.end.y, 0);
+
+        Handles.color = Color.white;
+        for (int i = 0; i < curve.path.Count - 1; ++i)
+        {
+            Handles.DrawLine(curve.path[i], curve.path[i + 1]);
+        }
+
+        curve.Refresh();
+        SceneView.RepaintAll();
+    }
+
+}
