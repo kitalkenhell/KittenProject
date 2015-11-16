77a05ab1b2c736615fd5b5b351d25ce5da4268e8
Marcin Łaszcz <ml@byss.mobi>
2015-11-16 16:05:22 +0100

    Scieżki krzywych

---
+++ Assets/External/PolygonBuilder/Curve.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Curve
+{
+    public Vector3 begin = Vector3.zero;
+    public Vector3 end = Vector3.up;
+    public Vector3 tangentBegin = Vector3.left;
+    public Vector3 tangentEnd = Vector3.right;
+    public int quality = 20;
+
+    public float lenght;
+    public List<Vector3> path = new List<Vector3>();
+    public List<float> additiveDistance = new List<float>();
+
+    public void Refresh()
+    {
+        path.Clear();
+        additiveDistance.Clear();
+
+        path.Add(begin);
+        lenght = 0;
+        
+        for (int i = 0; i < quality; i++)
+        {
+            path.Add(Evaluate((float)i / (float)quality));
+            lenght += Vector3.Distance(path[i], path[i + 1]);
+            additiveDistance.Add(lenght);
+        }
+
+        path.Add(end);
+        lenght += Vector3.Distance(path[path.Count - 1], path[path.Count - 2]);
+        additiveDistance.Add(lenght);
+    }
+
+    public void Reset()
+    {
+        begin = Vector3.zero;
+        end = Vector3.up;
+        tangentBegin = Vector3.left;
+        tangentEnd = Vector3.right;
+    }
+
+    public Vector3 PointOnCurve(float distance)
+    {
+
+        if (distance > lenght)
+        {
+            return end;
+        }
+        else if (distance <= 0)
+        {
+            return begin;
+        }
+
+        int index = additiveDistance.BinarySearch(distance);
+
+        if (index < 0)
+        {
+            index = ~index;
+        }
+
+        float distanceToGo = -(distance - additiveDistance[index]);
+
+        return Evaluate(additiveDistance[index] / lenght - distanceToGo / lenght);
+    }
+
+    public Vector3 Evaluate(float value)
+    {
+        float h1 = 2 * Pow3(value) - 3 * Pow2(value) + 1;
+        float h2 = -2 * Pow3(value) + 3 * Pow2(value);
+        float h3 = Pow3(value) - 2 * Pow2(value) + value;
+        float h4 = Pow3(value) - Pow2(value);
+        return h1 * begin + h2 * end + h3 * tangentBegin * 3 + h4 * -tangentEnd * 3;
+    }
+
+    float Pow3(float value)
+    {
+        return value * value * value;
+    }
+
+    float Pow2(float value)
+    {
+        return value * value;
+    }
+}
---
+++ Assets/External/PolygonBuilder/Editor/CurvePathEditor.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(CurvePath))]
+public class CCurvePathEditor : Editor
+{
+    public bool showPoints = true;
+    public bool showTangents = true;
+    public bool useTansformTool = false;
+
+    public override void OnInspectorGUI()
+    {
+        CurvePath path = (CurvePath)target;
+
+        if (GUILayout.Button("Reset"))
+        {
+            path.Reset();
+        }
+
+        if (GUILayout.Button("Add point"))
+        {
+            path.AddPoint();
+        }
+
+        showPoints = EditorGUILayout.Toggle("Show Points", showPoints);
+        showTangents = EditorGUILayout.Toggle("Show Tangents", showTangents);
+        useTansformTool = EditorGUILayout.Toggle("Use transform handle", useTansformTool);
+
+        path.quality = Mathf.Max(EditorGUILayout.IntField("Path Quality", path.quality), 1);
+    }
+
+    public void OnSceneGUI()
+    {
+        CurvePath path = (CurvePath)target;
+
+        Handles.SphereCap(0, path.handle, Quaternion.identity, 0.25f);
+
+        for (int i = 0; i < path.points.Count - 1; i++)
+        {
+            Curve curve = path.curves[i];
+            curve.quality = path.quality;
+
+            if (showPoints)
+            {
+                Handles.color = Color.white;
+
+                if (!useTansformTool)
+                {
+                    path.points[i] = curve.begin = Handles.FreeMoveHandle(curve.begin, Quaternion.identity, 0.25f, Vector3.zero, Handles.DotCap);
+                    path.points[i + 1] = curve.end = Handles.FreeMoveHandle(curve.end, Quaternion.identity, 0.25f, Vector3.zero, Handles.DotCap);
+                }
+                else
+                {
+                    path.points[i] = curve.begin = Handles.PositionHandle(curve.begin, Quaternion.identity);
+                    path.points[i + 1] = curve.end = Handles.PositionHandle(curve.end, Quaternion.identity);
+
+                    Handles.SphereCap(0, curve.begin, Quaternion.identity, 0.25f);
+                    Handles.SphereCap(0, curve.end, Quaternion.identity, 0.25f);
+                }
+
+                if (i != 0)
+                {
+                    path.curves[i - 1].end = path.points[i];
+                }
+                if (i >= path.curves.Count)
+                {
+                    path.curves[i + 1].begin = path.points[i + 1];
+                } 
+            }
+
+            if (showTangents)
+            {
+                Handles.color = Color.red;
+
+                if (!useTansformTool)
+                {
+                    curve.tangentBegin = Handles.FreeMoveHandle(curve.begin + curve.tangentBegin, Quaternion.identity, 0.25f, Vector3.one, Handles.DotCap) - curve.begin;
+                    curve.tangentEnd = Handles.FreeMoveHandle(curve.end + curve.tangentEnd, Quaternion.identity, 0.25f, Vector3.one, Handles.DotCap) - curve.end;
+                }
+                else
+                {
+                    curve.tangentBegin = Handles.PositionHandle(curve.begin + curve.tangentBegin, Quaternion.identity) - curve.begin;
+                    curve.tangentEnd = Handles.PositionHandle(curve.end + curve.tangentEnd, Quaternion.identity) - curve.end;
+
+                    Handles.SphereCap(0, curve.begin + curve.tangentBegin, Quaternion.identity, 0.25f);
+                    Handles.SphereCap(0,curve.end + curve.tangentEnd, Quaternion.identity, 0.25f);
+                }
+
+                Handles.DrawLine(curve.begin, curve.begin + curve.tangentBegin);
+                Handles.DrawLine(curve.end, curve.end + curve.tangentEnd); 
+            }
+
+            if (curve.path.Count >= 2)
+            {
+                Handles.color = Color.white;
+                for (int j = 0; j < curve.path.Count - 1; ++j)
+                {
+                    Handles.DrawLine(curve.path[j], curve.path[j + 1]);
+                }
+            }
+
+            curve.Refresh();
+        }
+
+        path.Refresh();
+        SceneView.RepaintAll();
+    }
+
+}
