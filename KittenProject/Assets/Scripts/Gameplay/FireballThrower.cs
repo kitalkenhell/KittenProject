a1404dc5fd8bf0726e2881177faf45b8a89a09f5
Marcin <marcinlaszcz@gmail.com>
2016-03-03 23:02:25 +0100

    piąta plansza

---
+++ KittenProject/Assets/Scripts/Gameplay/FireballThrower.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballThrower : MonoBehaviour
+{
+    const float forceMultiplier = 3.0f;
+
+    public MinMax spawnInterval;
+    public MinMax randomForceMultiplier;
+    public GameObject fireballPrefab;
+    public Transform fireballForce;
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        Rigidbody2D fireball;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval.Random());
+            fireball = (Instantiate(fireballPrefab, transform.position, Quaternion.identity) as GameObject).GetComponentInChildren<Rigidbody2D>();
+            fireball.velocity = (fireballForce.position - transform.position) * forceMultiplier * randomForceMultiplier.Random(); 
+        }
+    }
+}
