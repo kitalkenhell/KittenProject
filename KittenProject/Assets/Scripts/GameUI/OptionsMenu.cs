7e72a536ea31ec388bfcadcadefa5922e0ba724e
Gotard <kermajt@o2.pl>
2016-08-21 22:31:26 +0200

    Losowanie skórek Reklama po przejściu planszy

---
+++ KittenProject/Assets/Scripts/GameUI/OptionsMenu.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class OptionsMenu : MonoBehaviour
+{
+    const string scaleLabel = "UI Scale: ";
+
+    public Slider uiScaleSlider;
+    public Text uiScaleText;
+    public ControlsButtonsScale buttonScaler;
+    public PauseMenu pauseMenu;
+
+    Animator animator;
+
+    int toggleAnimHash;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+        toggleAnimHash = Animator.StringToHash("Toggle");
+    }
+
+    void OnEnable()
+    {
+        uiScaleSlider.value = GameSettings.UiScale;
+        uiScaleText.text = Strings.PauseMenu.uiScale + String.Format("{0:0.0}", uiScaleSlider.value);
+
+        animator.SetTrigger(toggleAnimHash);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GoBack();
+        }
+    }
+    public void OnUiScaleSlider(float scale)
+    {
+        GameSettings.UiScale = uiScaleSlider.value;
+        uiScaleText.text = Strings.PauseMenu.uiScale + String.Format("{0:0.0}", uiScaleSlider.value);
+        buttonScaler.RefreshButtons();
+    }
+
+    public void OnBackButtonClicked()
+    {
+        GoBack();
+    }
+
+    void GoBack()
+    {
+        pauseMenu.BackFromOptions();
+        animator.SetTrigger(toggleAnimHash);
+    }
+
+    public void OnHide()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- KittenProject/Assets/Scripts/Scriptable/PlayerItems.cs
+++ KittenProject/Assets/Scripts/Scriptable/PlayerItems.cs
@@ -62,4 +62,138 @@ public class PlayerItems : ScriptableObject
     {
         return GetParachuteSkin(GameSettings.PlayerParachuteSkin);
     }
+
+    public bool HasAllSkins()
+    {
+        return HasAllBodySkins() && HasAllHats() && HasAllParachutes();
+    }
+
+    public bool HasAllBodySkins()
+    {
+        foreach (var skin in bodySkins)
+        {
+            if (!PersistentData.IsHavingBodySkin(skin.itemName))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool HasAllHats()
+    {
+        foreach (var skin in hatSkins)
+        {
+            if (!PersistentData.IsHavingBodySkin(skin.itemName))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool HasAllParachutes()
+    {
+        foreach (var skin in parachuteSkins)
+        {
+            if (!PersistentData.IsHavingBodySkin(skin.itemName))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public PlayerBodySkin GetRandomNotUnlockedBodySkin()
+    {
+        int index = UnityEngine.Random.Range(0, bodySkins.Length);
+
+        if (PersistentData.IsHavingBodySkin(bodySkins[index].itemName))
+        {
+            for (int i = index + 1; i < bodySkins.Length; ++i)
+            {
+                if (!PersistentData.IsHavingBodySkin(bodySkins[i].itemName))
+                {
+                    return bodySkins[i];
+                }
+            }
+
+            for (int i = index - 1; i >= 0; --i)
+            {
+                if (!PersistentData.IsHavingBodySkin(bodySkins[i].itemName))
+                {
+                    return bodySkins[i];
+                }
+            }
+        }
+        else
+        {
+            return bodySkins[index];
+        }
+
+        return null;
+    }
+
+    public PlayerHatSkin GetRandomNotUnlockedHat()
+    {
+        int index = UnityEngine.Random.Range(0, hatSkins.Length);
+
+        if (PersistentData.IsHavingHatSkin(hatSkins[index].itemName))
+        {
+            for (int i = index + 1; i < hatSkins.Length; ++i)
+            {
+                if (!PersistentData.IsHavingHatSkin(hatSkins[i].itemName))
+                {
+                    return hatSkins[i];
+                }
+            }
+
+            for (int i = index - 1; i >= 0; --i)
+            {
+                if (!PersistentData.IsHavingHatSkin(hatSkins[i].itemName))
+                {
+                    return hatSkins[i];
+                }
+            }
+        }
+        else
+        {
+            return hatSkins[index];
+        }
+
+        return null;
+    }
+
+    public PlayerParachuteSkin GetRandomNotUnlockedParachute()
+    {
+        int index = UnityEngine.Random.Range(0, parachuteSkins.Length);
+
+        if (PersistentData.IsHavingParachuteSkin(parachuteSkins[index].itemName))
+        {
+            for (int i = index + 1; i < parachuteSkins.Length; ++i)
+            {
+                if (!PersistentData.IsHavingParachuteSkin(parachuteSkins[i].itemName))
+                {
+                    return parachuteSkins[i];
+                }
+            }
+
+            for (int i = index - 1; i >= 0; --i)
+            {
+                if (!PersistentData.IsHavingParachuteSkin(parachuteSkins[i].itemName))
+                {
+                    return parachuteSkins[i];
+                }
+            }
+        }
+        else
+        {
+            return parachuteSkins[index];
+        }
+
+        return null;
+    }
 }
